368e3c7ba8111cd19cd887347892de925330a74b
Aaron Dack <aaron@onprem.com>
2015-11-07 12:27:43 -0600

    Translating RNA into protein

--- computation.py
+++ computation.py
@@ -98,4 +98,31 @@ def hamming_distance(file):
             line2 = file_object.readline()
             data = [char_line for char_line,char_line2 in zip(line,line2) if char_line != char_line2]
             return(len(data))
-
+#
+# The 20 commonly occurring amino acids are abbreviated by using 20 letters from the English alphabet (all letters except for B, J, O, U, X, and Z). Protein strings are constructed from these 20 symbols. Henceforth, the term genetic string will incorporate protein strings along with DNA strings and RNA strings.
+#
+# The RNA codon table dictates the details regarding the encoding of specific codons into the amino acid alphabet.
+#
+# Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+#
+# Return: The protein string encoded by s.
+def translating_rna_to_protein(rna):
+    protein_string=""
+    codon_dict = {
+    "UUU":"F","UUC":"F","UUA":"L","UUG":"L","UCU":"S","UCC":"S","UCA":"S", "UCG":"S",
+    "UAU":"Y","UAC":"Y","UGU":"C","UGC":"C","UGG":"W",
+    "CUU":"L","CUC":"L","CUA":"L","CUG":"L","CCU":"P","CCC":"P","CCA":"P", "CCG":"P","CAU":"H",
+    "CAC":"H","CAA":"Q","CAG":"Q","CGU":"R","CGC":"R","CGA":"R", "CGG":"R","AUU":"I","AUC":"I",
+    "AUA":"I","AUG":"M","ACU":"T","ACC":"T","ACA":"T","ACG":"T","AAU":"N","AAC":"N","AAA":"K","AAG":"K",
+    "AGU":"S","AGC":"S","AGA":"R","AGG":"R","GUU":"V","GUC":"V","GUA":"V","GUG":"V","GCU":"A","GCC":"A",
+    "GCA":"A","GCG":"A","GAU":"D","GAC":"D","GAA":"E","GAG":"E","GGU":"G","GGC":"G","GGA":"G","GGG":"G"
+    }
+    with open(rna, 'r') as rnaObject:
+        for rna_string in rnaObject:
+                for nucleotide in range(0,len(rna_string),3):
+                   processing = rna_string[nucleotide:nucleotide+3]
+                   if processing in codon_dict:
+                            protein_string+=codon_dict[processing]
+                   elif processing == "UAA" or "UAG" or "UGA":
+                       break
+                return protein_string
